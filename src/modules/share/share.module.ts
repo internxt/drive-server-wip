de1ec826012a43c3e3381a2b06a6c2526e4a69c6
carlos.lopez <carlos.lopez@voicemod.net>
2022-06-09 11:01:25 +0200

    wip

--- src/modules/share/share.module.ts
+++ src/modules/share/share.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { File } from '../file/file.model';
+import { Folder } from '../folder/folder.model';
 import { ShareController } from './share.controller';
 import { Share } from './share.model';
 import { SequelizeShareRepository } from './share.repository';
 import { ShareService } from './share.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Share, File])],
+  imports: [SequelizeModule.forFeature([Share, File, Folder])],
   controllers: [ShareController],
   providers: [SequelizeShareRepository, ShareService],
 })
