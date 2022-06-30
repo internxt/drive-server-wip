afa54402ea31b8a1574104d27b76c82c64820b2b
Carlos Lopez <cloaction@gmail.com>
2022-06-30 13:33:24 +0200

    fix migrations

--- migrations/20220309190600-create-deleted-files-trigger.js
+++ migrations/20220309190600-create-deleted-files-trigger.js
@@ -1,15 +1,17 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
-      `CREATE TRIGGER if not exists xCloud.copy_deleted_files_on_delete BEFORE DELETE on xCloud.folders
+      `CREATE TRIGGER if not exists copy_deleted_files_on_delete BEFORE DELETE on folders
 			FOR EACH ROW
 			BEGIN
-			INSERT INTO xCloud.deleted_files(file_id, user_id, folder_id, bucket) select file_id, user_id, folder_id, bucket from xCloud.files where xCloud.files.folder_id = OLD.id;
+			INSERT INTO deleted_files(file_id, user_id, folder_id, bucket) select file_id, user_id, folder_id, bucket from files where files.folder_id = OLD.id;
 			END`,
     );
   },
 
   down: async (queryInterface) => {
-    await queryInterface.sequelize.query('drop trigger xCloud.copy_deleted_files_on_delete;');
+    await queryInterface.sequelize.query(
+      'drop trigger copy_deleted_files_on_delete;',
+    );
   },
 };
--- migrations/20220309190601-create-clear_orphan_folders_by_user-procedure.js
+++ migrations/20220309190601-create-clear_orphan_folders_by_user-procedure.js
@@ -2,16 +2,18 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(
       `
-      create or replace procedure xCloud.clear_orphan_folders_by_user(userid int(11))
+      create or replace procedure clear_orphan_folders_by_user(userid int(11))
       BEGIN
-        delete from xCloud.folders where parent_id is not null and parent_id not in (select id from xCloud.folders where user_id = userid) and user_id = userid;
-        select count(*) as total_left from xCloud.folders where parent_id is not null and parent_id not in (select id from xCloud.folders where user_id = userid) and user_id = userid;
+        delete from folders where parent_id is not null and parent_id not in (select id from folders where user_id = userid) and user_id = userid;
+        select count(*) as total_left from folders where parent_id is not null and parent_id not in (select id from folders where user_id = userid) and user_id = userid;
       END;
       `,
     );
   },
 
   down: async (queryInterface) => {
-    await queryInterface.sequelize.query('drop procedure xCloud.clear_orphan_folders_by_user;');
+    await queryInterface.sequelize.query(
+      'drop procedure clear_orphan_folders_by_user;',
+    );
   },
 };
