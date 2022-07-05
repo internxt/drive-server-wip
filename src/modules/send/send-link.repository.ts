50a69b6ec77d0163ffc4a03d9f3beca0cd2127b4
Carlos Lopez <cloaction@gmail.com>
2022-07-05 13:44:16 +0200

    fixes PR

--- src/modules/send/send-link.repository.ts
+++ src/modules/send/send-link.repository.ts
@@ -1,9 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { FileAttributes } from '../file/file.domain';
-import { User, UserAttributes } from '../user/user.domain';
+import { User } from '../user/user.domain';
 import { UserModel } from '../user/user.repository';
-import { FolderAttributes } from '../folder/folder.domain';
 import { SendLink, SendLinkAttributes } from './send-link.domain';
 import { SendLinkItem } from './send-link-item.domain';
 
@@ -19,7 +17,10 @@ import {
   HasMany,
   Sequelize,
 } from 'sequelize-typescript';
-import { fromBinary, toBinary } from '../../lib/binary-converter';
+import {
+  getStringFromBinary,
+  convertStringToBinary,
+} from '../../lib/binary-converter';
 
 @Table({
   underscored: true,
@@ -210,7 +211,7 @@ export class SequelizeSendRepository implements SendRepository {
     return SendLinkItem.build({
       id: model.id,
       type: model.type,
-      name: fromBinary(atob(model.name)),
+      name: getStringFromBinary(atob(model.name)),
       linkId: model.linkId,
       networkId: model.networkId,
       encryptionKey: model.encryptionKey,
@@ -223,7 +224,7 @@ export class SequelizeSendRepository implements SendRepository {
   private toModelItem(domain) {
     return {
       id: domain.id,
-      name: btoa(toBinary(domain.name)),
+      name: btoa(convertStringToBinary(domain.name)),
       type: domain.type,
       linkId: domain.linkId,
       networkId: domain.networkId,
