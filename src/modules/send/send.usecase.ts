149fc58e3d4d95c39b981a7b1e8c640c88616542
Carlos Lopez <cloaction@gmail.com>
2022-07-07 17:56:34 +0200

    Revert "Merge branch 'master' into feature/SEND-5-upload-folders"
    This reverts commit e5294f20c2548030385f48eb8b235f42f1cb2d7f, reversing
    changes made to ce765c15b712fe43b2243cacb3624daa37893852.

--- src/modules/send/send.usecase.ts
+++ src/modules/send/send.usecase.ts
@@ -26,7 +26,7 @@ export class SendUseCases {
     sendLink.addView();
     sendLink.updatedAt = now;
     await this.sendRepository.update(sendLink);
-    return await this.sendRepository.findById(id);
+    return sendLink;
   }
   async createSendLinks(
     user: User | null,
@@ -66,7 +66,7 @@ export class SendUseCases {
     await this.sendRepository.createSendLinkWithItems(sendLink);
 
     const sendLinkCreatedEvent = new SendLinkCreatedEvent({
-      sendLink: await this.sendRepository.findById(sendLink.id),
+      sendLink,
     });
 
     this.notificationService.add(sendLinkCreatedEvent);
