4946abbebe3ca85f509fb59edaea11bd9b83a359
Carlos Lopez <cloaction@gmail.com>
2022-07-22 10:07:06 +0200

    updated only folders table

--- migrations/20220722090000-add-columns-folders-files.js
+++ migrations/20220722090000-add-columns-folders-files.js
@@ -11,22 +11,10 @@ module.exports = {
       type: Sequelize.DATE,
       allowNull: true,
     });
-
-    await queryInterface.addColumn('files', 'deleted', {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    });
-    await queryInterface.addColumn('files', 'deleted_at', {
-      type: Sequelize.DATE,
-      allowNull: true,
-    });
   },
 
   async down(queryInterface) {
     await queryInterface.removeColumn('folders', 'deleted');
     await queryInterface.removeColumn('folders', 'deleted_at');
-    await queryInterface.removeColumn('files', 'deleted');
-    await queryInterface.removeColumn('files', 'deleted_at');
   },
 };
