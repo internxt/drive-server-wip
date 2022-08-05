e11d0e264b8dab33b2dfe99513d5202b4b59fbca
Alex Menor <alex.menor@icloud.com>
2022-08-05 19:37:25 +0200

    add orderBy param to list shares operation

--- src/lib/pagination.ts
+++ src/lib/pagination.ts
@@ -1,6 +1,6 @@
 export class Pagination {
   static calculatePagination(page: number, perPage: number) {
-    const offset = (page - 1) * perPage;
+    const offset = page * perPage;
     const limit = perPage;
 
     return { offset, limit };
