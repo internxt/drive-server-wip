da1be4930fa5a9e6f50958e38c59ab221e765ff0
Alex Menor <alex.menor@icloud.com>
2022-08-13 16:53:46 +0200

    use proper decorator to limit de length of the items array in MoveItemsToTrashDto

--- src/modules/trash/dto/controllers/move-items-to-trash.dto.ts
+++ src/modules/trash/dto/controllers/move-items-to-trash.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from 'class-transformer';
-import { IsEnum, IsNotEmpty, MaxLength } from 'class-validator';
+import { IsEnum, IsNotEmpty, ArrayMaxSize } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export enum ItemType {
@@ -28,7 +28,7 @@ export class MoveItemsToTrashDto {
   @ApiProperty({
     description: 'Array of items with files and folders ids',
   })
-  @MaxLength(50)
+  @ArrayMaxSize(50)
   @Type(() => ItemToTrash)
   items: ItemToTrash[];
 }
