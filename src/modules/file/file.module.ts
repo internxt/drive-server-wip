aaf68c30255216e8b615bb1926ce30f01297a430
Joan Vicens <jwcervera@hotmail.com>
2022-08-31 09:04:47 +0200

    Fix/clear trash on tras uses case (#50)
    feat: add clear trash endpoint

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { SequelizeFileRepository } from './file.repository';
 import { FileUseCases } from './file.usecase';
 import { FileModel } from './file.repository';
+import { ShareModel } from '../share/share.repository';
+import { ShareModule } from '../share/share.module';
+import { BridgeModule } from '../../externals/bridge/bridge.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([FileModel])],
+  imports: [
+    SequelizeModule.forFeature([FileModel, ShareModel]),
+    forwardRef(() => ShareModule),
+    BridgeModule,
+  ],
   controllers: [],
   providers: [SequelizeFileRepository, FileUseCases],
   exports: [FileUseCases],
--- src/modules/trash/trash.module.ts
+++ src/modules/trash/trash.module.ts
@@ -1,13 +1,26 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, Module, forwardRef } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { FileModule } from '../file/file.module';
 import { FolderModule } from '../folder/folder.module';
 import { NotificationModule } from '../../externals/notifications/notifications.module';
 import { UserModule } from '../user/user.module';
 import { TrashController } from './trash.controller';
+import { TrashUseCases } from './trash.usecase';
+import { ShareModule } from '../share/share.module';
+import { FileModel } from '../file/file.repository';
+import { ShareModel } from '../share/share.repository';
 
 @Module({
-  imports: [FileModule, FolderModule, NotificationModule, UserModule],
+  imports: [
+    SequelizeModule.forFeature([FileModel, ShareModel]),
+    forwardRef(() => FileModule),
+    forwardRef(() => ShareModule),
+    FolderModule,
+    NotificationModule,
+    UserModule,
+    ShareModule,
+  ],
   controllers: [TrashController],
-  providers: [Logger],
+  providers: [Logger, TrashUseCases],
 })
 export class TrashModule {}
