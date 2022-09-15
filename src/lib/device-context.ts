203d6d516f5742aa867a440faf39f7b2433774c8
Carlos Lopez <cloaction@gmail.com>
2022-09-15 12:44:44 +0200

    device-context fix

--- src/lib/device-context.ts
+++ src/lib/device-context.ts
@@ -1,25 +1,14 @@
-import DeviceDetector from 'node-device-detector';
+import DeviceDetector from 'device-detector-js';
 import { Logger } from '@nestjs/common';
 const logger = new Logger();
 
 export function getDeviceContextByUserAgent(userAgent: string) {
   try {
     const deviceDetector = new DeviceDetector();
-    const deviceDetected = deviceDetector.detect(userAgent);
+    const deviceDetected = deviceDetector.parse(userAgent);
     return {
-      os: {
-        version: deviceDetected.os.version,
-        name: deviceDetected.os.name,
-        short_name: deviceDetected.os.short_name,
-        family: deviceDetected.os.family,
-      },
-      device: {
-        type: deviceDetected.device.type,
-        manufacturer: deviceDetected.device.brand,
-        model: deviceDetected.device.model,
-        brand: deviceDetected.device.brand,
-        brand_id: deviceDetected.device.id,
-      },
+      os: deviceDetected.os,
+      device: deviceDetected.device,
       client: deviceDetected.client,
     };
   } catch (err) {
