46fb830c270cef853a4e01d0586433dd087ca5be
joan vicens <jwcervera@hotmail.com>
2022-09-19 10:52:15 +0200

    feat: add password to send links

--- migrations/20220919083117-add-password-to-shared-links.js
+++ migrations/20220919083117-add-password-to-shared-links.js
@@ -6,9 +6,16 @@ module.exports = {
       type: Sequelize.BLOB('medium'),
       allowNull: true,
     });
+
+    await queryInterface.addColumn('send_links', 'password', {
+      type: Sequelize.BLOB('medium'),
+      allowNull: true,
+    });
   },
 
   async down(queryInterface) {
     await queryInterface.removeColumn('shares', 'password');
+
+    await queryInterface.removeColumn('send_links', 'password');
   },
 };
