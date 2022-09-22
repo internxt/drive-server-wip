ce695393a29d15230885a77a40dcf143d42bbeb5
Joan Vicens <jwcervera@hotmail.com>
2022-09-22 16:10:03 +0200

    chore: type share response (#81)

--- src/modules/folder/folder.domain.ts
+++ src/modules/folder/folder.domain.ts
@@ -1,4 +1,5 @@
 import { User } from '../user/user.domain';
+import { FolderDto } from './dto/folder.dto';
 export interface FolderAttributes {
   id: number;
   parentId: number;
@@ -90,7 +91,7 @@ export class Folder implements FolderAttributes {
     this.user = user;
   }
 
-  toJSON() {
+  toJSON(): FolderDto {
     return {
       id: this.id,
       parent: this.parent,
