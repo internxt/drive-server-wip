94362a5841f3c724b7c973b07b493b6fdc050538
joan vicens <jwcervera@hotmail.com>
2022-09-22 10:37:27 +0200

    test: add integration test to share

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  INestApplication,
+  NotFoundException,
+  ValidationPipe,
+} from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { TransformInterceptor } from '../src/lib/transform.interceptor';
 import { HttpStatus } from '@nestjs/common';
+import { SequelizeShareRepository } from './../src/modules/share/share.repository';
+import { ShareMother } from './share.mother';
+import { UserMother } from './user.mother';
+import { SequelizeUserRepository } from './../src/modules/user/user.repository';
+import { Share } from './../src/modules/share/share.domain';
+import { NotificationService } from './../src/externals/notifications/notification.service';
+import { CryptoService } from '../src/externals/crypto/crypto.service';
+import { File } from '../src/modules/file/file.domain';
 
 // Running e2e tests require a database named "Xcloud_test" in the mariadb database first.
 describe('AppController (e2e)', () => {
@@ -194,6 +206,200 @@ describe('AppController (e2e)', () => {
           });
         });
       });
+    });
+  });
+});
+
+describe('Share Endpoints', () => {
+  let app: INestApplication;
+
+  const fakeNotificationService = {
+    add: (_: any) => {
+      //no op
+    },
+  } as unknown as NotificationService;
+
+  const fakeUserRepository = {
+    findByUsername: (_: string) => UserMother.create(),
+  } as unknown as SequelizeUserRepository;
+
+  describe('unprotected shares', () => {
+    const databaseShare = ShareMother.createWithPassword(null);
+    const databaseSharedFile = databaseShare.item as File;
+    const unprotectedShareRepository = {
+      findByToken: (token: string) => {
+        if (token === databaseShare.token) return databaseShare;
+        throw new NotFoundException('share not found');
+      },
+      update: (_: Share) => Promise.resolve(),
+    } as unknown as SequelizeShareRepository;
+
+    beforeAll(async () => {
+      if (process.env.NODE_ENV !== 'test') {
+        throw new Error('Cannot do E2E tests without NODE_ENV=test ');
+      }
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideProvider(SequelizeShareRepository)
+        .useValue(unprotectedShareRepository)
+        .overrideProvider(SequelizeUserRepository)
+        .useValue(fakeUserRepository)
+        .overrideProvider(NotificationService)
+        .useValue(fakeNotificationService)
+        .compile();
+      app = moduleFixture.createNestApplication();
+      app.useGlobalPipes(new ValidationPipe());
+      app.useGlobalInterceptors(new TransformInterceptor());
+      await app.init();
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('return the shared', async () => {
+      const expectedShare = {
+        id: databaseShare.id,
+        token: databaseShare.token,
+        mnemonic: databaseShare.mnemonic,
+        bucket: databaseShare.bucket,
+        isFolder: databaseShare.isFolder,
+        views: databaseShare.views,
+        timesValid: databaseShare.timesValid,
+        active: databaseShare.active,
+        // code: databaseShare.code,
+        createdAt: databaseShare.createdAt.toISOString(),
+        updatedAt: databaseShare.updatedAt.toISOString(),
+        // fileId: databaseShare.fileId,
+        // fileSize: databaseShare.fileSize,
+        // folderId: databaseShare.folderId,
+        // fileToken: databaseShare.fileToken,
+        item: {
+          ...databaseSharedFile,
+          createdAt: databaseSharedFile.createdAt.toISOString(),
+          modificationTime: databaseSharedFile.modificationTime.toISOString(),
+          updatedAt: databaseSharedFile.updatedAt.toISOString(),
+        },
+        encryptionKey: databaseShare.encryptionKey,
+        protected: false,
+      };
+
+      const response = await request(app.getHttpServer()).get(
+        `/storage/share/${databaseShare.token}`,
+      );
+
+      expect(response.status).toBe(HttpStatus.OK);
+      expect(response.body).toBe({
+        ...expectedShare,
+        views: expectedShare.views + 1,
+      });
+    });
+
+    it('returns resource not found if the share not exist', async () => {
+      const token = '1234';
+
+      const response = await request(app.getHttpServer()).get(
+        `/storage/share/${token}`,
+      );
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('protected shares', () => {
+    let cryptoService: CryptoService;
+
+    const password = 'KPit1mKILC';
+    const databaseShare = ShareMother.createWithPassword(password);
+    const databaseSharedFile = databaseShare.item as File;
+
+    const protectedShareRepository = {
+      findByToken: (_: string) => databaseShare,
+      update: (_: Share) => Promise.resolve(),
+    } as unknown as SequelizeShareRepository;
+
+    beforeAll(async () => {
+      if (process.env.NODE_ENV !== 'test') {
+        throw new Error('Cannot do E2E tests without NODE_ENV=test ');
+      }
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideProvider(SequelizeShareRepository)
+        .useValue(protectedShareRepository)
+        .overrideProvider(SequelizeUserRepository)
+        .useValue(fakeUserRepository)
+        .overrideProvider(NotificationService)
+        .useValue(fakeNotificationService)
+        .compile();
+      app = moduleFixture.createNestApplication();
+      app.useGlobalPipes(new ValidationPipe());
+      app.useGlobalInterceptors(new TransformInterceptor());
+      cryptoService = moduleFixture.get<CryptoService>(CryptoService);
+      await app.init();
+    });
+
+    afterAll(async () => {
+      await app.close();
+    });
+
+    it('return the shared when the correct passwrod is porvided', async () => {
+      const encryptedPassword = cryptoService.encryptText(password);
+      const expectedShare = {
+        id: databaseShare.id,
+        token: databaseShare.token,
+        mnemonic: databaseShare.mnemonic,
+        bucket: databaseShare.bucket,
+        isFolder: databaseShare.isFolder,
+        views: databaseShare.views,
+        timesValid: databaseShare.timesValid,
+        active: databaseShare.active,
+        // code: databaseShare.code,
+        createdAt: databaseShare.createdAt.toISOString(),
+        updatedAt: databaseShare.updatedAt.toISOString(),
+        // fileId: databaseShare.fileId,
+        // fileSize: databaseShare.fileSize,
+        // folderId: databaseShare.folderId,
+        // fileToken: databaseShare.fileToken,
+        item: {
+          ...databaseSharedFile,
+          createdAt: databaseSharedFile.createdAt.toISOString(),
+          modificationTime: databaseSharedFile.modificationTime.toISOString(),
+          updatedAt: databaseSharedFile.updatedAt.toISOString(),
+        },
+        encryptionKey: databaseShare.encryptionKey,
+        protected: true,
+      };
+
+      const response = await request(app.getHttpServer())
+        .get(`/storage/share/${databaseShare.token}`)
+        .set('x-send-password', encryptedPassword);
+
+      expect(response.status).toBe(HttpStatus.OK);
+      expect(response.body).toBe({
+        ...expectedShare,
+        views: expectedShare.views + 1,
+      });
+    });
+
+    it('return unauthorized response when password is not correct', async () => {
+      const token = 'mGc9eCtk8lnphJMV6SpTJmF9SYDV7x';
+      const encryptedPassword = '1234';
+
+      const response = await request(app.getHttpServer())
+        .get(`/storage/share/${token}`)
+        .set('x-send-password', encryptedPassword);
+
+      expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('returns unauthorized response when no password provided', async () => {
+      const token = 'whHG1saU0Ex0ilILPGhM';
+      const response = await request(app.getHttpServer()).get(
+        `/storage/share/${token}`,
+      );
+      expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
     });
   });
 });
