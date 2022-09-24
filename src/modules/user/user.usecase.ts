2ab91598c61815baf6fac85644dfb1fd89781ba0
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-09-24 16:04:10 +0200

    feat(users): add required services for signup + create domain errors

--- src/modules/user/user.usecase.ts
+++ src/modules/user/user.usecase.ts
@@ -1,13 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import { SequelizeUserRepository } from './user.repository';
-
-import { Environment } from '@internxt/inxt-js';
 import { ConfigService } from '@nestjs/config';
+import { Environment } from '@internxt/inxt-js';
+import { v4 } from 'uuid';
+
+import { SequelizeUserRepository } from './user.repository';
+import { UserAttributes } from './user.domain';
+import { CryptoService } from '../../externals/crypto/crypto';
+import { FolderUseCases } from '../folder/folder.usecase';
+import { BridgeService } from 'src/externals/bridge/bridge.service';
+import { InvitationAcceptedEvent } from 'src/externals/notifications/events/invitation-accepted.event';
+import { NotificationService } from 'src/externals/notifications/notification.service';
+import { Sign } from 'src/middlewares/passport';
+
+class InvalidReferralCodeError extends Error {
+  constructor() {
+    super('The referral code used is not correct');
+
+    Object.setPrototypeOf(this, InvalidReferralCodeError.prototype);
+  }
+}
+
+class UserAlreadyRegisteredError extends Error {
+  constructor(email: string) {
+    super(`User ${email || ''} is already registered`);
+
+    Object.setPrototypeOf(this, UserAlreadyRegisteredError.prototype);
+  }
+}
+
 @Injectable()
 export class UserUseCases {
   constructor(
     private userRepository: SequelizeUserRepository,
+    private folderUseCases: FolderUseCases,
     private configService: ConfigService,
+    private cryptoService: CryptoService,
+    private networkService: BridgeService,
+    private notificationService: NotificationService,
   ) {}
 
   getUserByUsername(email: string) {
