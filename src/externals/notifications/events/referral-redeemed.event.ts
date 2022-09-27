871335d7e14058869812af583563fd0082128659
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-09-27 09:56:29 +0200

    feat(users): referral redeemed event

---
+++ src/externals/notifications/events/referral-redeemed.event.ts
@@ -0,0 +1,20 @@
+import {
+  ReferralAttributes,
+  UserAttributes,
+} from 'src/modules/user/user.domain';
+import { Event } from './event';
+
+export class ReferralRedeemedEvent extends Event {
+  public uuid: UserAttributes['uuid'];
+  public referralKey: ReferralAttributes['key'];
+
+  constructor(
+    uuid: UserAttributes['uuid'],
+    referralKey: ReferralAttributes['key'],
+  ) {
+    super('referral.redeemed', {});
+
+    this.uuid = uuid;
+    this.referralKey = referralKey;
+  }
+}
