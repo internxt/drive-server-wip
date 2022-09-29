4249ac2383510eedc0a0c333efe4a607b356d68a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-09-29 07:00:11 +0200

    feat(users): log errors on user creation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Res,
+  Logger,
+  Req,
+} from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiOkResponse,
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 import { Public } from '../auth/decorators/public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UserUseCases } from './user.usecase';
+import {
+  InvalidReferralCodeError,
+  UserAlreadyRegisteredError,
+  UserUseCases,
+} from './user.usecase';
 
 @ApiTags('User')
 @Controller('users')
@@ -22,7 +35,11 @@ export class UserController {
   @ApiOkResponse({ description: 'Creates a user' })
   @ApiBadRequestResponse({ description: 'Missing required fields' })
   @Public()
-  async createUser(@Body() createUserDto: CreateUserDto) {
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+    @Req() request: Request,
+    @Res() response: Response,
+  ) {
     try {
       const response = await this.userUseCases.createUser(createUserDto);
 
@@ -36,7 +53,23 @@ export class UserController {
         uuid: response.uuid,
       };
     } catch (err) {
-      console.log(err);
+      if (err instanceof InvalidReferralCodeError) {
+        return response.status(400).send({ error: err.message });
+      }
+
+      if (err instanceof UserAlreadyRegisteredError) {
+        return response.status(409).send({ error: err.message });
+      }
+
+      new Logger().error(
+        `[AUTH/REGISTER] ERROR: ${
+          (err as Error).message
+        }, BODY ${JSON.stringify(request.body)}, STACK: ${
+          (err as Error).stack
+        }`,
+      );
+
+      return response.status(500).send({ error: 'Internal Server Error' });
     }
   }
 }
