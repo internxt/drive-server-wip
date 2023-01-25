03aeff9a75425e22f1abc4260957d9d6ce9fc0c3
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2023-01-25 13:29:39 +0100

    env: add private and public keys env variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -38,6 +38,10 @@ export default () => ({
     cryptoSecret: process.env.CRYPTO_SECRET,
     cryptoSecret2: process.env.CRYPTO_SECRET2,
     jwt: process.env.JWT_SECRET,
+    gateway: {
+      privateKey: process.env.JWT_GATEWAY_PRIVATE_KEY,
+      publicKey: process.env.JWT_GATEWAY_PUBLIC_KEY,
+    },
   },
   apis: {
     notifications: {
