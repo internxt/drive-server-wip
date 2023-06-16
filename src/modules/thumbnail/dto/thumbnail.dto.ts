5abdd53e37d340a440740844c20df7d8e6c38fb2
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-06-16 08:41:07 +0200

    Change some snake_case to camelCase, removed unnecessary controller and minor thumbnail repository changes

--- src/modules/thumbnail/dto/thumbnail.dto.ts
+++ src/modules/thumbnail/dto/thumbnail.dto.ts
@@ -3,11 +3,11 @@ export class ThumbnailDto {
   fileId: number;
   type: string;
   size: number;
-  bucket_id: string;
-  bucket_file: string;
-  encrypt_version: string;
-  created_at: Date;
-  updated_at: Date;
-  max_width: number;
-  max_height: number;
+  bucketId: string;
+  bucketFile: string;
+  encryptVersion: string;
+  createdAt: Date;
+  updatedAt: Date;
+  maxWidth: number;
+  maxHeight: number;
 }
