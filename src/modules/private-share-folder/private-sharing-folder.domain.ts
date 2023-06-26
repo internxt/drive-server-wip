880008c8c5ecd7a45747019fcf21e9a3147948b8
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-06-26 16:46:24 +0200

    refactor(private-sharing): use namings from the original design

--- src/modules/private-share-folder/private-sharing-folder.domain.ts
+++ src/modules/private-share-folder/private-sharing-folder.domain.ts
@@ -1,24 +1,24 @@
 export interface PrivateSharingFolderAttributes {
   id: string;
-  folderUuid: string;
-  ownerUuid: string;
-  sharedWithUuid: string;
-  encryptedKey: string;
+  folderId: string;
+  ownerId: string;
+  sharedWith: string;
+  encryptionKey: string;
 }
 
 export class PrivateSharingFolder implements PrivateSharingFolderAttributes {
   id: string;
-  folderUuid: string;
-  ownerUuid: string;
-  sharedWithUuid: string;
-  encryptedKey: string;
+  folderId: string;
+  ownerId: string;
+  sharedWith: string;
+  encryptionKey: string;
 
   constructor(attributes: PrivateSharingFolderAttributes) {
     this.id = attributes.id;
-    this.folderUuid = attributes.folderUuid;
-    this.ownerUuid = attributes.ownerUuid;
-    this.sharedWithUuid = attributes.sharedWithUuid;
-    this.encryptedKey = attributes.encryptedKey;
+    this.folderId = attributes.folderId;
+    this.ownerId = attributes.ownerId;
+    this.sharedWith = attributes.sharedWith;
+    this.encryptionKey = attributes.encryptionKey;
   }
 
   static build(
