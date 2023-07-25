0422415b15f2e45dc7431f945bca40f535edab05
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-07-25 16:32:04 +0200

    fix(user): enforce parameters on account recovery

--- src/modules/user/dto/recover-account.dto.ts
+++ src/modules/user/dto/recover-account.dto.ts
@@ -1,22 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class RecoverAccountDto {
   @ApiProperty({
     example: 'some_hashed_pass',
     description: 'New user pass hashed',
   })
+  @IsNotEmpty()
   password: string;
 
   @ApiProperty({
     example: 'some_salt',
     description: 'Hashed password salt',
   })
+  @IsNotEmpty()
   salt: string;
 
   @ApiProperty({
     example: 'some_encrypted_mnemonic',
     description: 'User mnemonic encrypted with the new pass',
   })
+  @IsNotEmpty()
   mnemonic: string;
 }
 
