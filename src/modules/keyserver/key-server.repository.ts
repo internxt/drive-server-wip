580a2870808422f447b3a9886e4ef440d1a0491d
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-07-25 16:29:15 +0200

    refactor(key-server): remove patched imports + extract model

--- src/modules/keyserver/key-server.repository.ts
+++ src/modules/keyserver/key-server.repository.ts
@@ -1,47 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import {
-  AutoIncrement,
-  Column,
-  DataType,
-  ForeignKey,
-  Model,
-  PrimaryKey,
-  Table,
-} from 'sequelize-typescript';
 import { UserAttributes } from '../user/user.attributes';
-import { UserModel } from '../user/user.model';
 import { KeyServer, KeyServerAttributes } from './key-server.domain';
+import { KeyServerModel } from './key-server.model';
 
-@Table({
-  underscored: true,
-  timestamps: true,
-  tableName: 'keyserver',
-})
-export class KeyServerModel extends Model implements KeyServerAttributes {
-  @PrimaryKey
-  @AutoIncrement
-  @Column
-  id: number;
-
-  @ForeignKey(() => UserModel)
-  @Column(DataType.INTEGER)
-  userId: number;
-
-  @Column(DataType.STRING(920))
-  publicKey: string;
-
-  @Column(DataType.STRING(1356))
-  privateKey: string;
-
-  @Column(DataType.STRING(476))
-  revocationKey: string;
-
-  @Column(DataType.STRING)
-  encryptVersion: string;
-}
-
-export interface KeyServerRepository {
+interface KeyServerRepository {
   findUserKeysOrCreate(
     userId: UserAttributes['id'],
     data: Partial<KeyServerAttributes>,
