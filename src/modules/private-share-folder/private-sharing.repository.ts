c7d526450b4bd80e5922a9a7bc01d17ca97dc467
Carlos González <gcarloseduardo1@gmail.com>
2023-07-30 21:10:37 -0400

    wip

--- src/modules/private-share-folder/private-sharing.repository.ts
+++ src/modules/private-share-folder/private-sharing.repository.ts
@@ -7,6 +7,7 @@ import { PrivateSharingFolder } from './private-sharing-folder.domain';
 import { User } from '../user/user.domain';
 import { PrivateSharingFolderRolesModel } from './private-sharing-folder-roles.model';
 import { PrivateSharingRole } from './private-sharing-role.domain';
+import { UserModel } from '../user/user.model';
 
 export interface PrivateSharingRepository {
   findByOwner(
@@ -84,7 +85,15 @@ export class SequelizePrivateSharingRepository
       where: {
         ownerId: userUuid,
       },
-      include: [FolderModel],
+      include: [
+        FolderModel,
+        {
+          model: UserModel,
+          foreignKey: 'ownerId',
+          as: 'owner',
+          attributes: ['uuid', 'email', 'name', 'lastname', 'avatar'],
+        },
+      ],
       order: orderBy,
       limit,
       offset,
@@ -103,7 +112,15 @@ export class SequelizePrivateSharingRepository
       where: {
         sharedWith: userUuid,
       },
-      include: [FolderModel],
+      include: [
+        FolderModel,
+        {
+          model: UserModel,
+          foreignKey: 'ownerId',
+          as: 'owner',
+          attributes: ['uuid', 'email', 'name', 'lastname', 'avatar'],
+        },
+      ],
       order: orderBy,
       limit,
       offset,
