547c7efef0036bfc51a5a5621c6b1b2cdee0c049
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-08-17 14:20:07 +0200

    test: add random real data generator to fixtures

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,20 +1,36 @@
 import { v4 } from 'uuid';
+import { Chance } from 'chance';
 
 import { Folder } from '../src/modules/folder/folder.domain';
 import { User } from '../src/modules/user/user.domain';
 import { PrivateSharingFolder } from '../src/modules/private-share-folder/private-sharing-folder.domain';
 
+const constants = {
+  BUCKET_ID_LENGTH: 24,
+};
+const randomDataGenerator = new Chance();
+
 function newFolder(owner?: User): Folder {
+  const randomCreatedAt = randomDataGenerator.date();
+
   return Folder.build({
-    id: Math.random(),
+    id: randomDataGenerator.natural(),
     uuid: v4(),
     name: 'folder',
-    parentId: 0,
-    userId: owner?.id ?? 0,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    bucket: '',
-    plainName: '',
+    parentId: randomDataGenerator.natural(),
+    userId: owner?.id || randomDataGenerator.natural(),
+    createdAt: randomCreatedAt,
+    updatedAt: new Date(
+      randomDataGenerator.date({
+        min: randomCreatedAt,
+      }),
+    ),
+    bucket: randomDataGenerator.hash({
+      length: constants.BUCKET_ID_LENGTH,
+    }),
+    plainName: randomDataGenerator.string({
+      length: 20,
+    }),
     encryptVersion: '03-aes',
     deleted: false,
     removed: false,
@@ -24,33 +40,35 @@ function newFolder(owner?: User): Folder {
 }
 
 function newUser(): User {
+  const randomEmail = randomDataGenerator.email();
+
   return User.build({
-    id: Math.random() * 999999,
+    id: randomDataGenerator.natural(),
     userId: '',
     name: 'John',
     lastname: 'Doe',
     uuid: v4(),
-    email: '',
-    username: '',
-    bridgeUser: '',
+    email: randomEmail,
+    username: randomEmail,
+    bridgeUser: randomEmail,
     password: '',
     mnemonic: '',
     referrer: v4(),
     referralCode: v4(),
     credit: 0,
     hKey: Buffer.from(''),
-    rootFolderId: 0,
+    rootFolderId: randomDataGenerator.natural(),
     errorLoginCount: 0,
     isEmailActivitySended: 0,
-    lastResend: new Date(),
-    syncDate: new Date(),
+    lastResend: randomDataGenerator.date(),
+    syncDate: randomDataGenerator.date(),
     welcomePack: false,
     registerCompleted: false,
     secret_2FA: '',
     backupsBucket: '',
     sharedWorkspace: false,
     tempKey: '',
-    avatar: '',
+    avatar: v4(),
   });
 }
 
@@ -64,9 +82,9 @@ function newPrivateSharingFolder(bindTo: {
     folderId: bindTo.folder?.uuid ?? v4(),
     ownerId: bindTo.owner?.uuid ?? v4(),
     sharedWith: bindTo.sharedWith?.uuid ?? v4(),
-    createdAt: new Date(),
+    createdAt: randomDataGenerator.date(),
     encryptionKey: '',
   });
 }
 
-export { newUser, newFolder, newPrivateSharingFolder };
+export { newUser, newFolder, newPrivateSharingFolder, constants };
