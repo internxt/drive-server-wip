0089bbaf0fc4701f34562943e4a63c4a1c73a6df
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-08-28 11:02:43 +0200

    feat(sharing): create, update role DTOs

---
+++ src/modules/sharing/dto/update-sharing-role.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+import { SharingRole } from '../sharing.domain';
+
+export class UpdateSharingRoleDto {
+  @ApiProperty({
+    example: '84f47d08-dc7c-43dc-b27c-bec4edaa9598',
+    description: 'New role id',
+  })
+  @IsNotEmpty()
+  roleId: SharingRole['roleId'];
+}
