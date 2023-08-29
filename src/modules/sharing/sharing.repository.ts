66052e7cb083d2be7b5cc7b227b9664a5da8ce77
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-08-29 11:32:28 +0200

    feat(sharing): add finders on repository

--- src/modules/sharing/sharing.repository.ts
+++ src/modules/sharing/sharing.repository.ts
@@ -8,11 +8,18 @@ import {
 } from './models';
 import { SharingRolesModel } from './models/sharing-roles.model';
 import {
+  Role,
   Sharing,
+  SharingAttributes,
   SharingInvite,
   SharingRole,
   SharingRoleAttributes,
 } from './sharing.domain';
+import { User } from '../user/user.domain';
+import { Folder } from '../folder/folder.domain';
+import { FolderModel } from '../folder/folder.model';
+import { UserModel } from '../user/user.model';
+import sequelize, { Op, WhereOptions } from 'sequelize';
 
 interface SharingRepository {
   getInvitesByItem(
@@ -23,7 +30,12 @@ interface SharingRepository {
   createSharing(sharing: Omit<Sharing, 'id'>): Promise<Sharing>;
   deleteInvite(invite: SharingInvite): Promise<void>;
 
-  findSharing(sharingId: Sharing['id']): Promise<Sharing | null>;
+  findAllSharing(
+    where: Partial<Sharing>,
+    offset: number,
+    limit: number,
+    orderBy?: [string, string][],
+  ): Promise<Folder[]>;
   deleteSharing(id: Sharing['id']): Promise<void>;
 
   findSharingRole(
@@ -34,7 +46,7 @@ interface SharingRepository {
     update: Partial<Omit<SharingRole, 'id'>>,
   ): Promise<void>;
 
-  findRoles(): Promise<RoleModel[]>;
+  findRoles(): Promise<Role[]>;
   deleteSharingRole(sharingRole: SharingRole): Promise<void>;
 }
 
@@ -54,9 +66,29 @@ export class SequelizeSharingRepository implements SharingRepository {
   ) {}
 
   findSharingRole(
-    sharingRoleId: SharingRole['id'],
+    sharingRoleId?: SharingRole['id'],
   ): Promise<SharingRole | null> {
     return this.sharingRoles.findByPk(sharingRoleId);
+  }
+
+  async findByOwnerOrSharedWithFolderId(
+    userId: User['uuid'],
+    itemId: Sharing['itemId'],
+    offset: number,
+    limit: number,
+    orderBy?: [string, string][],
+  ): Promise<Sharing[]> {
+    const privateFolderSharing = await this.sharings.findAll({
+      where: {
+        itemId,
+        [Op.or]: [{ ownerId: userId }, { sharedWith: userId }],
+      },
+      order: orderBy,
+      limit,
+      offset,
+    });
+
+    return privateFolderSharing.map((sharing) => sharing.get({ plain: true }));
   }
 
   async updateSharingRole(
@@ -66,14 +98,161 @@ export class SequelizeSharingRepository implements SharingRepository {
     await this.sharingRoles.update(update, { where: { id: sharingRoleId } });
   }
 
-  async findRoles(): Promise<RoleModel[]> {
-    return this.roles.findAll();
+  async findRoles(): Promise<Role[]> {
+    const roles = await this.roles.findAll();
+
+    return roles.map((r) => Role.build(r));
   }
 
-  async findSharing(sharingId: Sharing['id']): Promise<Sharing | null> {
+  async findSharingById(sharingId: Sharing['id']): Promise<Sharing | null> {
     const raw = await this.sharings.findByPk(sharingId);
 
     return Sharing.build(raw);
+  }
+
+  async findSharingRoles(where: Partial<SharingRole>): Promise<SharingRole[]> {
+    const raw = await this.sharingRoles.findAll({
+      where,
+    });
+
+    return raw.map((model) => model.get({ plain: true }));
+  }
+
+  async findSharingsWithRolesBySharedWith(
+    users: User[],
+  ): Promise<(SharingAttributes & { role: Role })[]> {
+    return this.findSharingsWithRoles({
+      sharedWith: {
+        [Op.in]: users.map((user) => user.uuid),
+      },
+    });
+  }
+
+  private async findSharingsWithRoles(
+    where: WhereOptions<Sharing>,
+  ): Promise<(SharingAttributes & { role: Role })[]> {
+    const sharingsInRaw = await this.sharings.findAll({
+      where,
+    });
+
+    const sharingRoles = await this.sharingRoles.findAll({
+      where: {
+        sharingId: {
+          [Op.in]: sharingsInRaw.map((s) => s.id),
+        },
+      },
+      include: [
+        {
+          model: RoleModel,
+          as: 'role',
+        },
+      ],
+    });
+
+    return sharingsInRaw.map((sharing) => {
+      return {
+        ...sharing.get({ plain: true }),
+        role: sharingRoles
+          .find((sr) => sr.sharingId === sharing.id)
+          .role.get({ plain: true }),
+      };
+    });
+  }
+
+  async findOneSharing(where: Partial<Sharing>): Promise<Sharing | null> {
+    const raw = await this.sharings.findOne({ where });
+
+    return Sharing.build(raw);
+  }
+
+  async findAllSharing(
+    where: Partial<Sharing>,
+    offset: number,
+    limit: number,
+    orderBy?: [string, string][],
+  ): Promise<Folder[]> {
+    const sharedFolders = await this.sharings.findAll({
+      where,
+      include: [
+        {
+          model: FolderModel,
+          where: {
+            deleted: false,
+            removed: false,
+          },
+        },
+        {
+          model: UserModel,
+          foreignKey: 'ownerId',
+          as: 'owner',
+          attributes: ['uuid', 'email', 'name', 'lastname', 'avatar'],
+        },
+      ],
+      order: orderBy,
+      limit,
+      offset,
+    });
+
+    return sharedFolders.map((folder) => folder.get({ plain: true }));
+  }
+
+  async findByOwnerAndSharedWithMe(
+    userId: User['uuid'],
+    offset: number,
+    limit: number,
+    orderBy?: [string, string][],
+  ): Promise<Sharing[]> {
+    const sharedFolders = await this.sharings.findAll({
+      where: {
+        [Op.or]: [{ ownerId: userId }, { sharedWith: userId }],
+      },
+      attributes: [
+        // TODO: to check if is necessary to show the encryption_key in this query
+        [
+          sequelize.literal(`MAX("SharingModel"."encryption_key")`),
+          'encryptionKey',
+        ],
+        [sequelize.literal(`MAX("SharingModel"."created_at")`), 'createdAt'],
+      ],
+      group: ['folder.id', 'folder->user.id', 'SharingModel.owner_id'],
+      include: [
+        {
+          model: FolderModel,
+          where: {
+            deleted: false,
+            removed: false,
+          },
+          include: [
+            {
+              model: UserModel,
+              foreignKey: 'userId',
+              as: 'user',
+              attributes: ['uuid', 'email', 'name', 'lastname', 'avatar'],
+            },
+          ],
+        },
+      ],
+      order: orderBy,
+      limit,
+      offset,
+    });
+
+    return sharedFolders.map((shared) => this.toDomain(shared));
+  }
+
+  private toDomain(model: SharingModel): Sharing {
+    const folder = model.folder.get({ plain: true });
+    const user = model.folder.user.get({ plain: true });
+    delete folder.user;
+
+    return Sharing.build({
+      ...model.get({ plain: true }),
+      folder: Folder.build({
+        ...folder,
+        parent: folder.parent ? Folder.build(folder.parent) : null,
+        user: user ? User.build(user) : null,
+      }),
+    });
   }
 
   async deleteSharing(id: Sharing['id']): Promise<void> {
