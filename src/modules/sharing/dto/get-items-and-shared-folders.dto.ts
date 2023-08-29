ddf2121798990ab0c85c160a98ca0bcbe8a98609
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-08-29 11:50:13 +0200

    feat(sharing): get items and shared folders DTO

---
+++ src/modules/sharing/dto/get-items-and-shared-folders.dto.ts
@@ -0,0 +1,47 @@
+import { User } from '../../../../src/modules/user/user.domain';
+import { Sharing } from '../sharing.domain';
+import { Folder } from '../../../../src/modules/folder/folder.domain';
+import { File } from '../../../../src/modules/file/file.domain';
+
+export interface GetItemsReponse {
+  folders: FolderWithSharedInfo[];
+  files: File[];
+  credentials: {
+    networkPass: User['userId'];
+    networkUser: User['bridgeUser'];
+  };
+  token: string;
+}
+
+export interface GetFoldersReponse {
+  items: FolderWithSharedInfo[];
+  credentials: {
+    networkPass: User['userId'];
+    networkUser: User['bridgeUser'];
+  };
+  token: string;
+}
+
+export interface GetFilesResponse {
+  items: FileWithSharedInfo[];
+  credentials: {
+    networkPass: User['userId'];
+    networkUser: User['bridgeUser'];
+  };
+  token: string;
+}
+export interface FolderWithSharedInfo extends Folder {
+  encryptionKey: Sharing['encryptionKey'] | null;
+  dateShared: Date | null;
+  sharedWithMe: boolean | null;
+  credentials: {
+    networkPass: User['userId'];
+    networkUser: User['bridgeUser'];
+  };
+}
+
+export interface FileWithSharedInfo extends File {
+  encryptionKey: Sharing['encryptionKey'] | null;
+  dateShared: Date | null;
+  sharedWithMe: boolean | null;
+}
