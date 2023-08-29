704235be777c5c152179731bf29da43142262de2
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-08-29 11:49:51 +0200

    refactor(sharing): remove unnecesary field from accept-invite DTO

--- src/modules/sharing/dto/accept-invite.dto.ts
+++ src/modules/sharing/dto/accept-invite.dto.ts
@@ -4,13 +4,6 @@ import { SharingInvite } from '../sharing.domain';
 
 export class AcceptInviteDto {
   @ApiProperty({
-    example: '84f47d08-dc7c-43dc-b27c-bec4edaa9598',
-    description: 'Id of the invitation',
-  })
-  @IsNotEmpty()
-  invitationId: SharingInvite['id'];
-
-  @ApiProperty({
     example: 'encryption_key',
     description: 'Encryption key (just in case the invitation is a request)',
   })
