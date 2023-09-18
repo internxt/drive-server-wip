b7fd901f5f63a75415fa594a7a0339464f9502ca
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-09-18 15:01:14 +0200

    feat(sharing): be able to create public sharings

---
+++ src/modules/sharing/dto/create-sharing.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Sharing } from '../sharing.domain';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateSharingDto {
+  @ApiProperty({
+    example: 'uuid',
+    description: 'The uuid of the item to share',
+  })
+  @IsNotEmpty()
+  itemId: Sharing['itemId'];
+
+  @ApiProperty({
+    example: 'file | folder',
+    description: 'The type of the resource to share',
+  })
+  @IsNotEmpty()
+  itemType: Sharing['itemType'];
+
+  @ApiProperty({
+    example: 'encryption_key',
+    description: 'Encryption key',
+  })
+  @IsNotEmpty()
+  encryptionKey: Sharing['encryptionKey'];
+
+  @ApiProperty({
+    example: 'encryption_algorithm',
+    description: 'Encryption algorithm',
+  })
+  @IsNotEmpty()
+  encryptionAlgorithm: Sharing['encryptionAlgorithm'];
+}
