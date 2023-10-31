59d70a1a17b47d71c2732652b2dfabab47648d87
Andres Pinto <andres.pintosantiso@gmail.com>
2023-10-31 13:39:49 -0400

    feat: pre create users and allow pre created user invitations for sharings

--- src/modules/user/dto/pre-create-user.dto.ts
+++ src/modules/user/dto/pre-create-user.dto.ts
@@ -9,47 +9,4 @@ export class PreCreateUserDto {
     description: 'Email of the new account',
   })
   email: UserAttributes['email'];
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example: '$2a$08$4SN2l.8dM0fSUTzni3i61u047Sr/R3ocJYxbxmKdEmGJcVOj1sHIi',
-    description: 'Hashed password',
-  })
-  password: UserAttributes['password'];
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example:
-      'test test test test test test test test test test test test test test test test test test test test test test test test',
-    description: 'The mnemonic used to derive encryption keys',
-  })
-  mnemonic: UserAttributes['mnemonic'];
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example: 'salt',
-    description: 'Salt',
-  })
-  salt: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example: '',
-    description: '',
-  })
-  privateKey: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example: '',
-    description: '',
-  })
-  publicKey: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    example: '',
-    description: '',
-  })
-  revocationKey: string;
 }
