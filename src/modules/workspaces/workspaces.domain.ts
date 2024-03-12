0050fec6ccfa659340293cefa96f586a2cf48c84
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2024-03-12 15:06:57 -0400

    Adding workspace repository and domain

---
+++ src/modules/workspaces/workspaces.domain.ts
@@ -0,0 +1,53 @@
+import { WorkspaceAttributes } from './attributes/workspace.attributes';
+
+export class Workspace implements WorkspaceAttributes {
+  id: string;
+  ownerId: string;
+  address: string;
+  name: string;
+  description: string;
+  defaultTeamId: string;
+  workspaceUserId: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor({
+    id,
+    ownerId,
+    address,
+    name,
+    description,
+    defaultTeamId,
+    workspaceUserId,
+    createdAt,
+    updatedAt,
+  }: WorkspaceAttributes) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.address = address;
+    this.name = name;
+    this.description = description;
+    this.defaultTeamId = defaultTeamId;
+    this.workspaceUserId = workspaceUserId;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  static build(user: WorkspaceAttributes): Workspace {
+    return new Workspace(user);
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      ownerId: this.ownerId,
+      address: this.address,
+      name: this.name,
+      description: this.description,
+      defaultTeamId: this.defaultTeamId,
+      workspaceUserId: this.workspaceUserId,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+}
