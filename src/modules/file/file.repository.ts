70013c2584fda0e131e24b19039dad3ea15c4c6f
Andres Pinto <apinto@shokworks.io>
2024-03-22 09:50:23 -0400

    mend

--- src/modules/file/file.repository.ts
+++ src/modules/file/file.repository.ts
@@ -443,7 +443,6 @@ export class SequelizeFileRepository implements FileRepository {
           },
           status: {
             [Op.eq]: FileStatus.EXISTS,
-            [Op.not]: FileStatus.DELETED,
           },
         },
       },
