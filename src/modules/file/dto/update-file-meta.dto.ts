4b18641e4347ef5dafeea77190c4af61f1caddd2
Andres Pinto <apinto@shokworks.io>
2024-05-31 00:47:17 -0400

    chore: added query values descriptions

--- src/modules/file/dto/update-file-meta.dto.ts
+++ src/modules/file/dto/update-file-meta.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UpdateFileMetaDto {
   @IsString()
+  @ApiProperty({
+    description: 'The plain text name of the file',
+    example: 'example',
+  })
   plainName: string;
 }
--- src/modules/folder/dto/update-folder-meta.dto.ts
+++ src/modules/folder/dto/update-folder-meta.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UpdateFolderMetaDto {
   @IsString()
+  @ApiProperty({
+    description: 'The plain text name of the Folder',
+    example: 'example',
+  })
   plainName: string;
 }
