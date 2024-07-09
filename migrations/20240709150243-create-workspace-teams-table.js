96ad1b3992d430e3936c686681336c833dfc2949
Andres Pinto <apinto@shokworks.io>
2024-07-09 11:15:57 -0400

    chore: added migrations to master branch

---
+++ migrations/20240709150243-create-workspace-teams-table.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const tableName = 'workspace_teams';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(tableName, {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      workspace_id: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        references: {
+          model: 'workspaces',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      manager_id: {
+        type: Sequelize.STRING(36),
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'uuid',
+        },
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(tableName);
+  },
+};
