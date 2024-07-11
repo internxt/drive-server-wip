b5f3a8145bbebc49f7a2f24676efb039d41201c4
Andres Pinto <apinto@shokworks.io>
2024-07-11 01:19:09 -0400

    chore: moved new models from workspaces module

---
+++ src/modules/workspaces/models/workspace-invite.model.ts
@@ -0,0 +1,54 @@
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  PrimaryKey,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+import { WorkspaceModel } from './workspace.model';
+import { WorkspaceInviteAttributes } from '../attributes/workspace-invite.attribute';
+
+@Table({
+  underscored: true,
+  timestamps: true,
+  tableName: 'workspaces_invites',
+})
+export class WorkspaceInviteModel
+  extends Model
+  implements WorkspaceInviteAttributes
+{
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @ForeignKey(() => WorkspaceModel)
+  @Column({ type: DataType.UUID, allowNull: false })
+  workspaceId: string;
+
+  @BelongsTo(() => WorkspaceModel, {
+    foreignKey: 'workspaceId',
+    targetKey: 'id',
+    as: 'workspace',
+  })
+  workspace: WorkspaceModel;
+
+  @Column({ type: DataType.UUID, allowNull: false })
+  invitedUser: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  encryptionAlgorithm: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  encryptionKey: string;
+
+  @Column({ type: DataType.BIGINT.UNSIGNED, allowNull: false })
+  spaceLimit: number;
+
+  @Column({ allowNull: false, defaultValue: DataType.NOW })
+  createdAt: Date;
+
+  @Column({ allowNull: false, defaultValue: DataType.NOW })
+  updatedAt: Date;
+}
---
+++ src/modules/workspaces/models/workspace.model.ts
@@ -0,0 +1,93 @@
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  PrimaryKey,
+  ForeignKey,
+  BelongsTo,
+  HasMany,
+  AllowNull,
+  HasOne,
+} from 'sequelize-typescript';
+import { UserModel } from '../../user/user.model';
+import { WorkspaceUserModel } from './workspace-users.model';
+import { WorkspaceAttributes } from '../attributes/workspace.attributes';
+import { WorkspaceTeamModel } from './workspace-team.model';
+import { FolderModel } from '../../folder/folder.model';
+
+@Table({
+  underscored: true,
+  timestamps: true,
+  tableName: 'workspaces',
+})
+export class WorkspaceModel extends Model implements WorkspaceAttributes {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @ForeignKey(() => UserModel)
+  @Column(DataType.UUID)
+  ownerId: string;
+
+  @BelongsTo(() => UserModel, {
+    foreignKey: 'ownerId',
+    targetKey: 'uuid',
+    as: 'owner',
+  })
+  owner: UserModel;
+
+  @Column(DataType.STRING)
+  address: string;
+
+  @Column(DataType.STRING)
+  name: string;
+
+  @Column(DataType.STRING)
+  description: string;
+
+  @Column(DataType.BOOLEAN)
+  setupCompleted: boolean;
+
+  @AllowNull
+  @Column(DataType.STRING)
+  avatar: string;
+
+  @ForeignKey(() => WorkspaceTeamModel)
+  @Column(DataType.UUID)
+  defaultTeamId: string;
+
+  @ForeignKey(() => FolderModel)
+  @Column(DataType.UUID)
+  rootFolderId?: string;
+
+  @HasOne(() => FolderModel, 'uuid')
+  rootFolder: FolderModel;
+
+  @BelongsTo(() => UserModel, {
+    foreignKey: 'defaultTeamId',
+    targetKey: 'id',
+    as: 'defaultTeam',
+  })
+  defaultTeam: WorkspaceTeamModel;
+
+  @ForeignKey(() => UserModel)
+  @Column(DataType.UUID)
+  workspaceUserId: string;
+
+  @BelongsTo(() => UserModel, {
+    foreignKey: 'workspaceUserId',
+    targetKey: 'uuid',
+    as: 'workpaceUser',
+  })
+  workpaceUser: UserModel;
+
+  @HasMany(() => WorkspaceUserModel)
+  workspaceUsers: WorkspaceUserModel[];
+
+  @Column
+  createdAt: Date;
+
+  @Column
+  updatedAt: Date;
+}
