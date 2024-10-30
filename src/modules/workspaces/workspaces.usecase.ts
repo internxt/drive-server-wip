49b7bfd1dcfdbc0207b5fb49cf33b6e5bc083f1f
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-10-30 15:11:07 +0100

    fix: revert wrong changes

--- src/modules/workspaces/workspaces.usecase.ts
+++ src/modules/workspaces/workspaces.usecase.ts
@@ -567,7 +567,7 @@ export class WorkspacesUsecases {
 
     const newSpaceLimit = changeAssignedSpace.spaceLimit;
     const currentSpaceLimit = member.spaceLimit;
-    const limitDifference = currentSpaceLimit - newSpaceLimit;
+    const limitDifference = newSpaceLimit - currentSpaceLimit;
     const spaceLeftWithoutUser = spaceLeft + currentSpaceLimit;
 
     if (newSpaceLimit > spaceLeftWithoutUser) {
@@ -585,7 +585,7 @@ export class WorkspacesUsecases {
     await this.adjustOwnerStorage(
       workspaceId,
       Math.abs(limitDifference),
-      limitDifference > 0 ? 'ADD' : 'DEDUCT',
+      limitDifference > 0 ? 'DEDUCT' : 'ADD',
     );
 
     member.spaceLimit = changeAssignedSpace.spaceLimit;
