23d5d48d4c2d3714d21487f363b9baa0c5c54020
Andres Pinto <apinto@shokworks.io>
2025-02-04 12:57:21 -0400

    chore: added replace pre created user tests

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -136,6 +136,7 @@ export class UserController {
           response.user.email,
           response.user.uuid,
           keys.ecc.publicKey,
+          keys.kyber?.publicKey,
         );
       }
 
@@ -310,6 +311,7 @@ export class UserController {
           userCreated.user.email,
           userCreated.user.uuid,
           keys.ecc.publicKey,
+          keys.kyber?.publicKey,
         );
       }
 
--- src/modules/user/user.usecase.ts
+++ src/modules/user/user.usecase.ts
@@ -80,6 +80,7 @@ import {
   encryptMessageWithPublicKey,
   generateNewKeys,
 } from '../../externals/asymmetric-encryption/openpgp';
+import { SharingInvite } from '../sharing/sharing.domain';
 
 export class ReferralsNotAvailableError extends Error {
   constructor() {
@@ -469,14 +470,23 @@ export class UserUseCases {
       preCreatedUser;
 
     const privateKey = aes.decrypt(encPrivateKey, defaultPass);
-    const privateKyberKey = aes.decrypt(encPrivateKyberKey, defaultPass);
+    const privateKyberKey = encPrivateKyberKey
+      ? aes.decrypt(encPrivateKyberKey, defaultPass)
+      : null;
 
     const invites = await this.sharingRepository.getInvitesBySharedwith(
       preCreatedUser.uuid,
     );
 
+    const invitesToUpdate: SharingInvite[] = [];
+
     for (const invite of invites) {
       const { encryptionKey } = invite;
+
+      if (invite.isHybrid() && (!newPublicKyberKey || !privateKyberKey)) {
+        await this.sharingRepository.deleteInvite(invite);
+        continue;
+      }
 
       const decryptedEncryptionKey =
         await this.asymmetricEncryptionService.hybridDecryptMessageWithPrivateKey(
@@ -499,9 +509,11 @@ export class UserUseCases {
       invite.encryptionKey = newEncryptedEncryptionKey;
 
       invite.sharedWith = newUserUuid;
+
+      invitesToUpdate.push(invite);
     }
 
-    await this.sharingRepository.bulkUpdate(invites);
+    await this.sharingRepository.bulkUpdate(invitesToUpdate);
 
     await this.replacePreCreatedUserWorkspaceInvitations(
       preCreatedUser.uuid,
