567d8fcfec62b1165857fb6fd2cd7f8ebaa8a01a
ArceDanielShok <jarce@shokworks.io>
2025-03-21 12:59:13 -0300

    feat: enhance GetWorkspaceFilesQueryDto with Swagger API properties

--- src/modules/workspaces/dto/get-workspace-files.dto.ts
+++ src/modules/workspaces/dto/get-workspace-files.dto.ts
@@ -7,11 +7,13 @@ import {
 } from 'class-validator';
 import { FileStatus, SortableFileAttributes } from '../../file/file.domain';
 import { BasicPaginationDto } from '../../../common/dto/basic-pagination.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 const allowedStatuses = [...Object.values(FileStatus), 'ALL'];
 
 export class GetWorkspaceFilesQueryDto extends BasicPaginationDto {
   @IsOptional()
+  @ApiProperty()
   @IsEnum(allowedStatuses, {
     message: (args: ValidationArguments) =>
       `Invalid status provided: ${
@@ -20,18 +22,30 @@ export class GetWorkspaceFilesQueryDto extends BasicPaginationDto {
   })
   status?: FileStatus | 'ALL';
 
+  @ApiProperty({
+    required: false,
+  })
   @IsOptional()
   @IsString()
   bucket?: string;
 
+  @ApiProperty({
+    required: false,
+  })
   @IsOptional()
   sort?: SortableFileAttributes;
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsEnum(['ASC', 'DESC'], { message: 'Invalid order' })
   order?: 'ASC' | 'DESC';
 
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   @IsDateString({}, { message: 'Invalid date format for updatedAt' })
   updatedAt?: string;
 }
