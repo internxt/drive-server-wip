450ebb0d0af1c8d3cd7831fd44dbce76f50c3dcf
Daniel Jiménez Rivera <dani@internxt.com>
2025-03-25 17:14:03 +0100

    Create device dto

--- src/modules/backups/dto/create-device-as-folder.dto.ts
+++ src/modules/backups/dto/create-device-as-folder.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateDeviceAsFolderDto {
+  @ApiProperty()
   @IsString()
   deviceName: string;
 }
