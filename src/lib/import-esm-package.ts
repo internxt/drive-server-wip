04e6709f00dc7df43937fc8aaa9daccadfed01e6
Andres Pinto <apinto@shokworks.io>
2025-04-16 11:08:49 -0400

    fix: changed import esm util

--- src/lib/import-esm-package.ts
+++ src/lib/import-esm-package.ts
@@ -2,9 +2,22 @@
  * Dynamically imports an ESM package in a CommonJS NestJS project,
  * avoiding TypeScript transpilation to `require()`, which ESM packages do not support.
  */
-export const importEsmPackage = async <ReturnType>(
+/* export const importEsmPackage = async <ReturnType>(
   packageName: string,
 ): Promise<ReturnType> =>
   new Function(`return import('${packageName}')`)().then(
     (loadedModule: unknown) => loadedModule['default'] ?? loadedModule,
-  );
+  ); */
+
+export const importEsmPackage = async <ReturnType>(
+  packageName: string,
+): Promise<ReturnType> => {
+  try {
+    const modulePromise = eval(`import('${packageName}')`);
+    const module = await modulePromise;
+    return (module.default || module) as ReturnType;
+  } catch (error) {
+    console.error(`Error importing ESM package ${packageName}:`, error);
+    throw error;
+  }
+};
