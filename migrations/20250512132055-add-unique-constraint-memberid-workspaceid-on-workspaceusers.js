b2b4c0770241b9950ed43fcad124233e46f6b473
jzunigax2 <125698953+jzunigax2@users.noreply.github.com>
2025-05-12 09:23:18 -0600

    refactor: replace unique constraint migration for workspace_users and add new constraint for workspace_teams_users

---
+++ migrations/20250512132055-add-unique-constraint-memberid-workspaceid-on-workspaceusers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const tableName = 'workspace_users';
+const partialIndexName = 'workspace_users_active_member_workspace_index';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.query(`
+      CREATE UNIQUE INDEX ${partialIndexName}
+      ON ${tableName} (member_id, workspace_id)
+      WHERE deactivated = false;
+    `);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(
+      `DROP INDEX IF EXISTS ${partialIndexName};`,
+    );
+  },
+};
