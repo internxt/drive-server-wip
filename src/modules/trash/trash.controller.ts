35e7dd95a101a4968047fba532746b392be8f81c
jzunigax2 <125698953+jzunigax2@users.noreply.github.com>
2025-05-27 07:55:30 -0600

    chore: fix code smells refactor imports for multer-s3 integration

--- src/modules/trash/trash.controller.ts
+++ src/modules/trash/trash.controller.ts
@@ -39,10 +39,8 @@ import { File, FileStatus, SortableFileAttributes } from '../file/file.domain';
 import logger from '../../externals/logger';
 import { v4 } from 'uuid';
 import { HttpExceptionFilter } from '../../lib/http/http-exception.filter';
-import {
-  WorkspaceResourcesAction,
-  WorkspacesInBehalfGuard,
-} from '../workspaces/guards/workspaces-resources-in-behalf.decorator';
+import { WorkspaceResourcesAction } from '../workspaces/guards/workspaces-resources-in-behalf.types';
+import { WorkspacesInBehalfGuard } from '../workspaces/guards/workspaces-resources-in-behalf.decorator';
 import { GetDataFromRequest } from '../../common/extract-data-from-request';
 import { StorageNotificationService } from '../../externals/notifications/storage.notifications.service';
 import { BasicPaginationDto } from '../../common/dto/basic-pagination.dto';
