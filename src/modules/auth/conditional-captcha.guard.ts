309665c38fc38074ca3c4c6df07f6a6b04af05fa
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-07-03 15:24:59 +0200

    feat: improve error handling in conditional captcha guard

--- src/modules/auth/conditional-captcha.guard.ts
+++ src/modules/auth/conditional-captcha.guard.ts
@@ -8,6 +8,8 @@ import {
 } from '@nestjs/common';
 import { UserUseCases } from '../user/user.usecase';
 import { CaptchaService } from '../../externals/captcha/captcha.service';
+import { ClientHeaders } from './decorators/client.decorator';
+import { ClientEnum } from 'src/common/enums/platform.enum';
 
 @Injectable()
 export class ConditionalCaptchaGuard implements CanActivate {
@@ -21,18 +23,27 @@ export class ConditionalCaptchaGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const captchaToken = request.headers?.['x-internxt-captcha'];
+    const inxtClient =
+      request.headers[ClientHeaders.CLIENT_ID] ??
+      request.headers[ClientHeaders.CLIENT] ??
+      '';
     const ip = request.ips.length ? request.ips[0] : request.ip;
     const email = request.body?.email;
 
     if (!email) {
-      throw new BadRequestException(
-        'Email is required to evaluate captcha condition',
-      );
+      throw new BadRequestException('Email is required');
     }
 
     const { errorLoginCount } = await this.userUseCase.findByEmail(email);
 
     if (errorLoginCount >= this.threshold) {
+      if (inxtClient !== ClientEnum['Web']) {
+        throw new ForbiddenException({
+          message: 'too much failed attempts, go to the web app to login',
+          code: 'WebLoginRequired',
+        });
+      }
+
       if (!captchaToken) {
         throw new ForbiddenException({
           message: 'Captcha is required after multiple failed attempts',
