faf7ab8eb816c77629443cf5e2bb2b99edb094b9
Andres Pinto <apinto@shokworks.io>
2025-07-03 13:13:23 -0400

    feat: added default values to key, hostname and folder

--- migrations/20250703043221-add-key-and-folder-to-devices-table.js
+++ migrations/20250703043221-add-key-and-folder-to-devices-table.js
@@ -3,41 +3,37 @@
 const tableName = 'devices';
 const keyColumn = 'key';
 const folderUuidColumn = 'folder_uuid';
+const hostnameColumn = 'hostname';
 const macColumn = 'mac';
-const uniqueConstraintName = 'devices_key_user_platform_unique';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn(tableName, keyColumn, {
       type: Sequelize.STRING,
-      allowNull: true,
+      defaultValue: 'UNKNOWN_KEY',
     });
+
+    await queryInterface.addColumn(tableName, hostnameColumn, {
+      type: Sequelize.STRING,
+      defaultValue: 'UNKNOWN_HOSTNAME',
+    });
+
     await queryInterface.addColumn(tableName, folderUuidColumn, {
       type: Sequelize.UUID,
-      allowNull: true,
+      defaultValue: '00000000-0000-0000-0000-000000000000',
     });
+
     await queryInterface.changeColumn(tableName, macColumn, {
       type: Sequelize.STRING,
       allowNull: true,
     });
-
-    await queryInterface.addConstraint(tableName, {
-      fields: [keyColumn, 'userId', 'platform'],
-      type: 'unique',
-      name: uniqueConstraintName,
-      where: {
-        [keyColumn]: {
-          [Sequelize.Op.ne]: null,
-        },
-      },
-    });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeConstraint(tableName, uniqueConstraintName);
     await queryInterface.removeColumn(tableName, folderUuidColumn);
     await queryInterface.removeColumn(tableName, keyColumn);
+    await queryInterface.removeColumn(tableName, hostnameColumn);
 
     await queryInterface.changeColumn(tableName, macColumn, {
       type: Sequelize.STRING,
