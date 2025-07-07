5f1c690c3b30fdff3b37254af3e5eec62b6f0f3b
Andres Pinto <apinto@shokworks.io>
2025-07-07 16:46:49 -0400

    refactor: use try catch on notification listener

--- src/externals/notifications/listeners/notification.listener.ts
+++ src/externals/notifications/listeners/notification.listener.ts
@@ -29,32 +29,33 @@ export class NotificationListener {
       clientId: event.clientId,
       userId: event.userId,
     };
-    const res = await this.http
-      .post(apiNotificationURL, eventData, {
+
+    try {
+      const res = await this.http.post(apiNotificationURL, eventData, {
         headers,
-      })
-      .catch((err) => {
-        const errorData = {
-          message: err.message,
-          stack: err.stack,
-        };
+      });
 
-        if (isAxiosError(err)) {
-          errorData['url'] = err.config?.url;
-          errorData['agent'] = err.config?.httpsAgent?.options;
-          errorData['reusedSocket'] = err.request?.socket?.reusedSocket;
-        }
-
-        Logger.error(
-          `[NOTIFICATIONS_ERROR] Error in event ${event.name}, message: ${errorData.message}. Data: ${JSON.stringify(eventData, null, null)}, error: ${JSON.stringify(errorData, null, null)},`,
+      if (res && res.status !== 201) {
+        Logger.warn(
+          `Post to notifications service failed with status ${
+            res.status
+          }. Data: ${JSON.stringify(eventData, null, null)}`,
           this.constructor.name,
         );
-      });
-    if (res && res.status !== 201) {
-      Logger.warn(
-        `Post to notifications service failed with status ${
-          res.status
-        }. Data: ${JSON.stringify(eventData, null, null)}`,
+      }
+    } catch (error) {
+      const errorData = {
+        message: error.message,
+        stack: error.stack,
+      };
+
+      if (isAxiosError(error)) {
+        errorData['url'] = error.config?.url;
+        errorData['agent'] = error.config?.httpsAgent?.options;
+      }
+
+      Logger.error(
+        `[NOTIFICATIONS_ERROR] Error in event ${event.name}, message: ${errorData.message}. Data: ${JSON.stringify(eventData, null, null)}, error: ${JSON.stringify(errorData, null, null)},`,
         this.constructor.name,
       );
     }
