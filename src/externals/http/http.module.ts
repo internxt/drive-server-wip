a4d69eb648fc6e679b1106f43120308b11e53dc3
Andres Pinto <apinto@shokworks.io>
2025-07-08 20:02:56 -0400

    chore: increase sockets timeout to 16 secs

--- src/externals/http/http.module.ts
+++ src/externals/http/http.module.ts
@@ -13,6 +13,7 @@ const agentConfig: HttpsOptions | HttpOptions = {
   keepAlive: true,
   maxSockets: 100,
   maxFreeSockets: 30,
+  timeout: 16000,
   freeSocketTimeout: 4000, // Set this value to prevent socket hang up errors as Nodejs timeout is 5000ms
 };
 
