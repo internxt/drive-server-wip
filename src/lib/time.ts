ba7f08ab89a239c63dfef1757b833d08fef7d07a
Andres <143480783+apsantiso@users.noreply.github.com>
2025-08-28 23:33:46 -0400

    refactor(time): use dayjs for better date operations (#683)

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -4,18 +4,8 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
 export class Time {
-  private static freeze: Date | null = null;
-
   public static now(initialDate?: string | Date | number): Date {
-    if (Time.freeze !== null) {
-      return new Date(Time.freeze.getTime());
-    }
-
-    if (initialDate) {
-      return new Date(initialDate);
-    }
-
-    return new Date();
+    return dayjs(initialDate).utc().toDate();
   }
 
   public static dateWithDaysAdded(days: number, initialDate?: Date): Date {
@@ -23,25 +13,12 @@ export class Time {
     return date.add(days, 'day').toDate();
   }
 
-  public static setTime(now: Date): void {
-    Time.freeze = now;
-  }
-
-  public static stopTime(): Date {
-    Time.freeze = new Date();
-    return Time.freeze;
-  }
-
-  public static resumeTime(): void {
-    Time.freeze = null;
-  }
-
   public static readonly isToday = (date: Date): boolean => {
     return dayjs(date).utc().isSame(dayjs().utc(), 'day');
   };
 
   public static readonly convertTimestampToDate = (timestamp: number): Date => {
-    return new Date(timestamp * 1000);
+    return dayjs(timestamp * 1000).toDate();
   };
 
   /**
