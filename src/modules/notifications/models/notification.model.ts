94e275a25cf33f1552603edd1864e7e4368aa6d5
Andres <143480783+apsantiso@users.noreply.github.com>
2025-09-03 10:08:39 -0400

    Revert "Revert "[PB-4839] feat/notifications kickoff (#684)" (#692)" (#693)
    This reverts commit 1558bf759d5f84512ed30679c1ea20c0aec12779.

---
+++ src/modules/notifications/models/notification.model.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  Model,
+  Table,
+  PrimaryKey,
+  DataType,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+import { NotificationAttributes } from '../domain/notification.domain';
+
+@Table({
+  underscored: true,
+  timestamps: true,
+  tableName: 'notifications',
+})
+export class NotificationModel extends Model implements NotificationAttributes {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  id: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  link: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  message: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  targetType: string;
+
+  @AllowNull(true)
+  @Column(DataType.STRING)
+  targetValue: string | null;
+
+  @AllowNull(true)
+  @Column(DataType.DATE)
+  expiresAt: Date | null;
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  createdAt: Date;
+
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  updatedAt: Date;
+}
