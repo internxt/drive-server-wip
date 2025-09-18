55849082c1599731db87280b852d815adbb04376
douglas <douglas@internxt.com>
2025-09-18 06:21:03 -0500

    [PB-4491] chore: remove Rudderstack integration from drive-server-wip

--- src/externals/notifications/notifications.module.ts
+++ src/externals/notifications/notifications.module.ts
@@ -5,7 +5,6 @@ import { NotificationListener } from './listeners/notification.listener';
 import { HttpClientModule } from '../http/http.module';
 import { MailerModule } from '../mailer/mailer.module';
 import { SendLinkListener } from './listeners/send-link.listener';
-import { AnalyticsListener } from './listeners/analytics.listener';
 import { AuthListener } from './listeners/auth.listener';
 import { NewsletterService } from '../newsletter';
 import { StorageNotificationService } from './storage.notifications.service';
@@ -30,7 +29,6 @@ import { UserNotificationTokensModel } from '../../modules/user/user-notificatio
     NotificationService,
     NotificationListener,
     StorageNotificationService,
-    AnalyticsListener,
     SendLinkListener,
     AuthListener,
     NewsletterService,
