56072f4ed2b600409464622517440709069d5e8b
Andres Pinto <andres.pintosantiso+github@gmail.com>
2025-12-16 00:24:45 +0100

    fix: use no valid to skip the initial scan

--- migrations/20251212160000-add-constraint-empty-fileid-zero-size.js
+++ migrations/20251212160000-add-constraint-empty-fileid-zero-size.js
@@ -5,11 +5,13 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(
       `
-        ALTER TABLE files
+      ALTER TABLE files
         ADD CONSTRAINT check_fileid_null_only_for_zero_size
         CHECK (
-          (file_id IS NOT NULL AND file_id != '') OR size = 0
-        );
+          (size = 0 AND file_id IS NULL) OR 
+          (size > 0 AND file_id IS NOT NULL AND file_id != '')
+        )
+        NOT VALID;
       `,
     );
   },
