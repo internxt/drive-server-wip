b8baa7c7fc528c5fe4b83987dac635b25d69bff0
Sergio Gutiérrez <gv.sergio@gmail.com>
2026-01-08 16:44:59 +0100

    feat(files): add BRIN index for getting deleted files updated_at on a range fast and efficiently

---
+++ migrations/20260108150853-create-optimum-index-files-updatedat-status.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_files_deleted_updatedat_brin
+      ON files USING brin (updated_at)
+      WHERE status = 'DELETED';
+    `);
+    await queryInterface.sequelize.query(`DROP INDEX CONCURRENTLY files_status_updatedat_key;`);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY files_status_updatedat_key 
+      ON files (status, updated_at) 
+      WHERE status = 'DELETED';
+    `);
+    await queryInterface.sequelize.query(`DROP INDEX CONCURRENTLY idx_files_deleted_updatedat_brin;`);
+  }
+};
