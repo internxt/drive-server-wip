8c7ef425c96efe579738938243304990f681fcad
Andres <143480783+apsantiso@users.noreply.github.com>
2026-03-10 10:06:27 +0100

    set deployment date to 2026-03-09 (#985)

--- src/modules/trash/trash-expiration.utils.ts
+++ src/modules/trash/trash-expiration.utils.ts
@@ -1,7 +1,7 @@
 import { Time } from '../../lib/time';
 
 // Deployment of the feature
-export const TRASH_EXPIRATION_START_DATE = new Date('2026-03-10T00:00:00Z');
+export const TRASH_EXPIRATION_START_DATE = new Date('2026-03-09T00:00:00Z');
 
 export function calculateTrashExpirationDate(
   retentionDays: number,
