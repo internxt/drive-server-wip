87e379c628b579e334b6605464c11214c655c1fa
Andres Pinto <143480783+apsantiso@users.noreply.github.com>
2026-03-26 13:23:31 +0100

    chore: add files to delete log

--- src/modules/jobs/tasks/hard-delete-old-files/hard-delete-old-files.processor.ts
+++ src/modules/jobs/tasks/hard-delete-old-files/hard-delete-old-files.processor.ts
@@ -51,6 +51,8 @@ export class HardDeleteOldFilesProcessor extends WorkerHost {
         BATCH_SIZE,
       );
 
+      this.logger.log({ uuids }, 'files to delete');
+
       if (uuids.length === 0) break;
 
       const deletedCount =
