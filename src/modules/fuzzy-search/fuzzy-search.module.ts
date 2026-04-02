95a1e32ec6c93437582944360715e0e77263581c
Andres <143480783+apsantiso@users.noreply.github.com>
2026-04-02 12:55:44 +0200

    refactor(look_up): remove look up table references (#1024)

--- src/modules/fuzzy-search/fuzzy-search.module.ts
+++ src/modules/fuzzy-search/fuzzy-search.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { LookUpModel } from './look-up.model';
 import { SequelizeLookUpRepository } from './look-up.repository';
 import { FuzzySearchUseCases } from './fuzzy-search.usecase';
 import { FuzzySearchController } from './fuzzy-search.controller';
 
 @Module({
-  imports: [SequelizeModule.forFeature([LookUpModel])],
   controllers: [FuzzySearchController],
   providers: [SequelizeLookUpRepository, FuzzySearchUseCases],
   exports: [FuzzySearchUseCases, SequelizeLookUpRepository],
