8ca2e4e0fb238813dbb638955ca4887bef380ad4
Andres Pinto <143480783+apsantiso@users.noreply.github.com>
2026-04-16 17:45:55 +0200

    fix: fixed migration

---
+++ migrations/20260416154502-create-user-email-index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS users_email_idx
+      ON public.users(email);
+    `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP INDEX CONCURRENTLY IF EXISTS users_email_idx;
+    `);
+  }
+};
