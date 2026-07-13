42259c6b531eb993b3a809ba8c6aacca404718aa
Victor Fernandez Robles <victorfernandezrobles.dev@gmail.com>
2026-07-13 22:41:18 +0200

    [PB-6606] refactor: derive favorite DTOs from listing DTOs to remove duplication

--- src/modules/file/dto/get-favorite-files.dto.ts
+++ src/modules/file/dto/get-favorite-files.dto.ts
@@ -1,33 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
-import { type SortableFileAttributes } from '../file.domain';
-import { SortOrder } from '../../../common/order.type';
-import { RequiredLargePaginationDto } from '../../../common/dto/basic-pagination.dto';
+import { OmitType } from '@nestjs/swagger';
+import { GetFilesDto } from './get-files.dto';
 
-export class GetFavoriteFilesDto extends RequiredLargePaginationDto {
-  @ApiProperty({
-    description: 'Field to sort by',
-    enum: ['updatedAt', 'uuid'],
-    required: false,
-  })
-  @IsOptional()
-  @IsEnum(['updatedAt', 'uuid'])
-  sort?: SortableFileAttributes;
-
-  @ApiProperty({
-    description: 'Sort order',
-    enum: SortOrder,
-    required: false,
-  })
-  @IsOptional()
-  @IsEnum(SortOrder)
-  order?: SortOrder;
-
-  @ApiProperty({
-    description: 'Filter favorite files updated after this date',
-    required: false,
-  })
-  @IsOptional()
-  @IsString()
-  updatedAt?: string;
-}
+export class GetFavoriteFilesDto extends OmitType(GetFilesDto, [
+  'status',
+  'bucket',
+] as const) {}
