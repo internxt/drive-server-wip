ab5006bff7c3559732ef9897d1913ec1d1505486
Andres Pinto <143480783+apsantiso@users.noreply.github.com>
2026-08-10 09:17:08 +0200

    chore: added better explanation about down process

--- migrations/20260807120001-step-3-part-2-swap-pk-to-uuid.js
+++ migrations/20260807120001-step-3-part-2-swap-pk-to-uuid.js
@@ -8,15 +8,15 @@ module.exports = {
     await queryInterface.sequelize.query(`
         ALTER TABLE files DROP CONSTRAINT files_pkey CASCADE;
         ALTER TABLE files ADD CONSTRAINT files_pkey PRIMARY KEY USING INDEX ux_files_uuid_pk;
-        ALTER TABLE files ADD CONSTRAINT ux_files_id UNIQUE USING INDEX ux_files_id_idx;
         ALTER TABLE thumbnails ADD CONSTRAINT thumbnails_file_id_fkey
           FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE ON UPDATE CASCADE NOT VALID;
+        ALTER TABLE files ADD CONSTRAINT ux_files_id UNIQUE USING INDEX ux_files_id_idx;
       `);
   },
 
   async down(queryInterface, Sequelize) {
     // There's no safe down automatic migration for this. It requires an INDEX creation and a PK swap.
-    // You can manually restore the ID as PK by running the following SQL command:
+    // If the last step was not executed, you can manually restore the ID as PK by running the following SQL command:
     // ALTER TABLE files ADD CONSTRAINT files_pkey PRIMARY KEY USING INDEX ux_files_id_idx;
     // It leverages the previously created unique index on the id column.
   }
