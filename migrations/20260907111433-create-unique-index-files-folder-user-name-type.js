bb836fc1e3e672f09f5e08fc05d9ab63e9696543
Andres Pinto <143480783+apsantiso@users.noreply.github.com>
2026-09-07 15:48:48 +0200

    chore: create index for folder navigation (#1157)

---
+++ migrations/20260907111433-create-unique-index-files-folder-user-name-type.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    // Enforce uniqueness and matches the collation used in the navigation queries
+    await queryInterface.sequelize.query(`
+      CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_files_folder_user_name_type_unique_numeric
+      ON files (folder_uuid, user_id, plain_name COLLATE "custom_numeric", type)
+      WHERE status = 'EXISTS';
+    `);
+
+    // These indexes are redundant now
+    await queryInterface.sequelize.query(
+      'DROP INDEX CONCURRENTLY IF EXISTS idx_files_folder_user_exists;',
+    );
+    await queryInterface.sequelize.query(
+      'DROP INDEX CONCURRENTLY IF EXISTS files_plainname_type_folderid_exists_unique;',
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(`
+      CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_files_folder_user_exists
+      ON files (folder_uuid, user_id)
+      WHERE status = 'EXISTS';
+    `);
+    await queryInterface.sequelize.query(`
+      CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS files_plainname_type_folderid_exists_unique
+      ON files USING btree (plain_name, type, folder_id)
+      WHERE (status = 'EXISTS');
+    `);
+    await queryInterface.sequelize.query(
+      'DROP INDEX CONCURRENTLY IF EXISTS idx_files_folder_user_name_type_unique_numeric;',
+    );
+  },
+};
